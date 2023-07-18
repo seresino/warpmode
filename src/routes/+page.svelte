55cfbeb7ca164721a55cdeaed3a18a75161eb836
seresino <rubyseresin@icloud.com>
2023-07-18 11:33:08 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<div class="page">
+  <h1>Welcome to SvelteKit</h1>
+  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+</div>
+
+<style>
+  :global(body) {
+  background-color: black;
+}
+
+  h1 {
+    color: white;
+  }
+</style>
